14f44f5edf71cc14a1d682d9cbebd209ad24ecd0
samlior <samlior@foxmail.com>
2025-03-07 15:28:17 +0800

    Remove gas price oracle

--- packages/tx-plugin/src/gasEstimator/factory.ts
+++ packages/tx-plugin/src/gasEstimator/factory.ts
@@ -2,17 +2,12 @@ import { CHAIN_ID } from '@derivation-tech/context';
 import { EthGasEstimator } from './estimator';
 import { DefaultEthGasEstimator } from './default';
 import { LineaGasEstimator } from './linea';
-import { OpStackGasEstimator } from './opStack';
 
 export class EthGasEstimatorFactory {
     static getGasEstimator(chainId: CHAIN_ID): EthGasEstimator {
         switch (chainId) {
             case CHAIN_ID.LINEA:
                 return new LineaGasEstimator();
-            case CHAIN_ID.OPTIMISM:
-            case CHAIN_ID.BASE:
-            case CHAIN_ID.MANTLE:
-                return new OpStackGasEstimator();
             default:
                 return new DefaultEthGasEstimator();
         }
